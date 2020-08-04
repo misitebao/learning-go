abb941e949ff962b61ed8340fd4f1b71c1230687
misitebao <i@misitebao.com>
2020-08-05 01:44:25 +0800

    feat:新增 变量

---
+++ 语法基础/变量/example/variable4.go
@@ -0,0 +1,19 @@
+// 变量重新赋值
+
+package main 
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// 声明并初始化变量
+	var i int = 1
+	fmt.Println(i);  //output 1
+
+	// 变量重新赋值
+	i = 2
+	fmt.Println(i);  //output 2
+
+}
